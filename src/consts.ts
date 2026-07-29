cafdc4a2f7f3352745f6e2cb22bc720a7c5d6108
Reynor-0 <107984354+Reynor-0@users.noreply.github.com>
2026-07-29 17:51:43 +0800

    feat:前端展示优化

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Reynor's Blog";
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '记录学习过程中的知识、遇到的问题，以及一步步找到答案的过程。';
